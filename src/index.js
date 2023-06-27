767adf4cfd3aeb00482e06e0d37aa33dcacfe1af
Frank Wesdijk <frank.wesdijk@gmail.com>
2023-06-27 12:43:31 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const favStad = <h1>Mijn favoriete stad is Enschede!</h1>;
-const favBoek = (
-	<h2>Mijn favoriete boek is 'Kanonnen van Augustus' van Barbara Tuchmann</h2>
+const favStad = <h1>Enschede</h1>;
+const favBoek = <h1>Kanonnen van augustus</h1>;
+const element = (
+	<h2>
+		Mijn favoriete stad is {favStad}, mijn favoriete boek is {favBoek}
+	</h2>
 );
-ReactDOM.render(favStad + " " + favBoek, document.getElementById("divResult"));
+ReactDOM.render(element, document.getElementById("divResult"));
